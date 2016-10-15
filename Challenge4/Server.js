a30fc7e65461436174e5f1435afab999b577b112
pojdrovic <pojdrovic@gmail.com>
2016-10-15 16:30:08 -0400

    Update Server.js

--- Challenge4/Server.js
+++ Challenge4/Server.js
@@ -3,24 +3,25 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
-router.use(function (req, res, next) {
-	console.log("/" + req.method);
-	next();
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
 
-router.get("/", function(req,res){
-	res.sendFile(path + "index.html");
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
 });
 
 router.get("/about",function(req,res){
-	  res.sendFile(path + "about.html");
+  res.sendFile(path + "about.html");
 });
 
 app.use("/",router);
 
-app.use("*", function(req,res){
-	res.sendFile(path + "404.html");
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
 });
 
 app.listen(3000,function(){
-	console.log("Live at port 3000");
+  console.log("Live at Port 3000");
+});
