28c39009b08483c28a4ca20835354a51f32bf695
pojdrovic <Petar-PC@Petars-MacBook-Pro.local>
2016-10-29 18:07:45 -0400

    LIDAR globals

--- Challenge5/arduinoPID.ino
+++ Challenge5/arduinoPID.ino
@@ -3,6 +3,13 @@
 #include <Servo.h>
 #include <PID_v1.h>
 
+
+#define LIDAR_ADDRESS 0x62
+#define LIDAR_CONTROL_ADDRESS 0x00
+#define LIDAR_CONTROL_VALUE 0x04
+#define LIDAR_RANGE_ADDRESS 0x8f
+
+
 // Servo for turning the car and servo for reference.
 Servo steering_wheels;
 Servo ESC;
@@ -49,17 +56,26 @@ PID speed_PID(&speed_in, &speed_out, &reference_speed, sKp, sKi, sKd, DIRECT);
 
 void setup(){
   // Controls steering
-  steering_wheels.attach(1);
+  steering_wheels.attach(D1);
 
   // Controls speed
-  ESC.attach(2);
+  ESC.attach(D2);
 
   // Calibrate speed control
   calibrateESC();
 
+  Wire.begin();
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+
+
   angle_PID.SetMode(AUTOMATIC);
   control_PID.SetMode(AUTOMATIC);
   speed_PID.SetMode(AUTOMATIC);
+
+  angle_PID.SetOutputLimits(-45,45);
+  control_PID.SetOutputLimits(-45,45);
+  speed_PID.SetOutputLimits(-60,60);
 
 }
 
