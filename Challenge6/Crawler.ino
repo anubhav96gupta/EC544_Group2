115f2d012d3872ac9708c519fb73d368b5e82774
anubhav96gupta <anubhavg@bu.edu>
2016-11-05 20:52:29 -0400

    Crawler Aggregator Code

---
+++ Challenge6/Crawler.ino
@@ -0,0 +1,46 @@
+#include <XBee.h>
+#include <SoftwareSerial.h>
+
+XBee xbee = XBee();
+
+SoftwareSerial xbeeSerial(2,3);
+
+uint8_t payload[4] = {'T','E','S','T'};
+
+ZBRxResponse rx = ZBRxResponse();
+ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+
+void setup()
+{
+  Serial.begin(9600);
+  xbeeSerial.begin(9600);
+  xbee.setSerial(xbeeSerial);
+}
+
+void loop()
+{       
+    packetSend();
+    packetrecieve();
+}
+
+void packetSend()
+{ 
+  ZBTxRequest zbTx = ZBTxRequest(0xFFFF, payload, sizeof(payload));
+  xbee.send(zbTx);
+}
+
+void packetrecieve()
+{
+  xbee.readPacket(500);
+  uint8_t value = 0;
+  if (xbee.getResponse().isAvailable())
+  {          
+      if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
+      {
+        xbee.getResponse().getZBRxResponse(rx);
+        for (int i = 0; i < rx.getDataLength(); i++)
+          Serial.println( rx.getData(i) );
+      }
+  }
+}
+
