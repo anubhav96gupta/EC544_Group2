4a54595710923bed6c7d19b4c10366c72c312ca4
cmk16 <cmk91@bu.edu>
2016-11-29 14:29:08 -0500

    Update Ultrasonic.ino

--- Challenge8/Ultrasonic.ino
+++ Challenge8/Ultrasonic.ino
@@ -1,28 +1,24 @@
 
 const int PWPin = 5;
-long pulse1, sensor1 , cms;
+long pulse, sensor , cms;
 
 void setup () {
   Serial.begin(9600);
   pinMode(PWPin, INPUT);
 }
 
-void read_sensor(){
+void read_sensor()
+{
   pulse = pulseIn(PWPin, HIGH);
-  sensor = pulse1/147;
+  sensor = pulse/147;
   cms = sensor*2.54;
+  Serial.println(cms) ;
 }
 
-void printall(){         
-  Serial.print("S1");
-  Serial.print(" ");
-  Serial.print(cms);
-  Serial.println(" ");
 }
 
 void loop () {
   read_sensor();
-  printall();
   delay(1000);
 }
 
